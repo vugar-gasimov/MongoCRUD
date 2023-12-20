fb30b2507545739bd5f46397aa6e63f3d55aff54
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-20 21:48:43 +0200

    I moved DB ADMIN to process env

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const PORT = 3000;
-const { DB_ADMIN } = require("./config");
-
+const { DB_ADMIN } = process.env;
+// const { DB_ADMIN } = require("./config");
+// console.log(process.env.DB_ADMIN);
 mongoose
   .connect(DB_ADMIN)
   .then(() => {
